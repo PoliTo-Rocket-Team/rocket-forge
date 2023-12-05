b1d3068507c041b9bec6d6c21d7ee3b4e518fd81
alessio26gas <alessio26gas@libero.it>
2023-12-05 23:17:22 +0100

    Merged and cleaned correction factors

--- main.m
+++ main.m
@@ -3,6 +3,5 @@ clc
 
 run matlab/input_data
 run matlab/load_rocketcea
-run matlab/viscous
 run matlab/corr_factors
 run matlab/delivered
--- matlab/corr_factors.m
+++ matlab/corr_factors.m
@@ -2,17 +2,20 @@
 disp('Computing correction factors...')
 
 % Finite reaction rate combustion factor
-% er1 = (h0 / rt)^a * (pSL / pc)^b * log10(re /rt);
+% er1 = (h0 / rt)^a * (pSL / pc)^b * log10(re /rt); % RPA empirical
+% Is_kin = Is_vac_frozen + 0.66 * (Is_vac - Is_vac_frozen);
+% z_r = Is_kin / Is_vac * (1 - er2);
+er1 = 0.34 - 0.34 * Is_vac_frozen / Is_vac;
 er2 = max(0, 0.021 - 0.01 * log(pc / 2 / 10^6));
-% z_r = (1 - er1) * (1 - er2);
-
-Is_kin = Is_vac_frozen + 0.66 * (Is_vac - Is_vac_frozen);
-z_r = Is_kin / Is_vac * (1 - er2);
+z_r = (1 - er1) * (1 - er2);
 
 % Multi-phase loss factor
 z_zw = 1 - Z * Cs / we^2 * (Tc - Te * (1 + log(Tc / Te)));
 z_zt = 1 - Z / 2;
 z_z = 0.2 * z_zw + 0.8 * z_zt;
+
+% Friction loss factor
+z_f = 0.997732 - 0.403077 * (pc * rt) ^ (-0.5598);
 
 % Divergence loss factor
 alpha = atan((re - rt) / Le);
