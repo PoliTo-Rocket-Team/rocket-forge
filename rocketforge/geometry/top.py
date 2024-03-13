1be0b76c071dc119785c2ac4e588c1c87c9b4950
alessio26gas <alessio26gas@libero.it>
2024-03-13 18:08:50 +0100

    refactor: comments and function descriptions

--- rocketforge/geometry/top.py
+++ rocketforge/geometry/top.py
@@ -1,19 +1,16 @@
-from math import *
 from numpy import *
-from scipy.optimize import fsolve
 
 
 def get_divergent(At, RnOvRt, Le, thetan, thetae, eps):
+    '''
+    This function returns the x and y coordinates of the divergent for a thrust-optimized parabolic (TOP) nozzle
+    '''
+    Rt = sqrt(At/pi)        # Throat radius
+    Rn = RnOvRt * Rt        # Divergent circular arc radius
+    Re = Rt * sqrt(eps)     # Exit radius
 
-    Rt = sqrt(At/pi)
-    Rn = RnOvRt * Rt
-    Re = Rt * sqrt(eps)
-    Nt = Rn * sin(thetan)
-
-    x1 = linspace(0, Nt, 1000)
-    y1 = lambda x: - sqrt(Rn**2 - x**2) + Rt + Rn
-
-    xN = Nt
+    # Canted parabola contour
+    xN = Rn * sin(thetan)
     yN = Rt + Rn*(1-cos(thetan))
     xE = Le
     yE = Re
@@ -23,10 +20,14 @@ def get_divergent(At, RnOvRt, Le, thetan, thetae, eps):
     alpha=lambda theta: DN(theta)/(DN(theta)-DE(theta))
     xp= lambda theta: xN+alpha(theta)*(2-alpha(theta))*(xA-xN)+alpha(theta)**2 *(xE-xA)
     yp= lambda theta: yN+tan(theta)*(xp(theta)-xN)+alpha(theta)*DN(theta)
-
     theta = linspace(thetan, thetae, 1000)
     x2 = xp(theta)
 
+    # Divergent circular arc coordinates
+    x1 = linspace(0, xN, 1000)
+    y1 = lambda x: - sqrt(Rn**2 - x**2) + Rt + Rn
+
+    # Concatenate coordinates vectors
     x = concatenate((x1, x2))
     y = concatenate((y1(x1), yp(theta)))
 
@@ -34,8 +35,19 @@ def get_divergent(At, RnOvRt, Le, thetan, thetae, eps):
 
 
 def lc15(At, RnOvRt, eps):
+    '''
+    This function returns the length of a conical nozzle with a 15 degrees divergence angle
+    '''
+    # Throat radius
     Rt = sqrt(At/pi)
+
+    # Divergent circular arc radius
     Rn = RnOvRt * Rt
+
+    # Exit radius
     Re = Rt * sqrt(eps)
+
+    # Conical nozzle length
     Lc15 = (Re - Rt - Rn * (1-cos(radians(15))))/tan(radians(15)) + Rn*sin(radians(15))
+
     return Lc15
--- rocketforge/utils/helpers.py
+++ rocketforge/utils/helpers.py
@@ -1,7 +1,10 @@
 from customtkinter import CTkEntry, CTkTextbox
 
 
-def updateentry(entry: CTkEntry, value, disabled=False):
+def updateentry(entry: CTkEntry, value: any, disabled: bool=False) -> None:
+    '''
+    This function is used to update CTkEntry contents
+    '''
     entry.configure(state="normal")
     entry.delete("0", "200")
     if type(value) == str:
@@ -12,7 +15,10 @@ def updateentry(entry: CTkEntry, value, disabled=False):
         entry.configure(state="disabled")
 
 
-def updatetextbox(textbox: CTkTextbox, value, disabled=False):
+def updatetextbox(textbox: CTkTextbox, value: any, disabled: bool=False) -> None:
+    '''
+    This function is used to update CTkTextbox contents
+    '''
     textbox.configure(state="normal")
     textbox.delete("0.0", "200.0")
     textbox.insert("0.0", value)
