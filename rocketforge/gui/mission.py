112ea62c0f44d2ac3e7c3364686fff307f51098b
alessio26gas <alessio26gas@libero.it>
2024-11-05 23:32:59 +0100

    feat: rail definition GUI

--- rocketforge/gui/mission.py
+++ rocketforge/gui/mission.py
@@ -26,6 +26,14 @@ class MissionFrame(ctk.CTkFrame):
         self.envbutton.place(anchor="e", relx=0.48, rely=0.11, x=0, y=0)
         self.envwindow = None
 
+        self.raillabel = CTkLabel(self, text="Rail is not configured")
+        self.raillabel.place(anchor="w", relx=0.52, rely=0.11, x=0, y=0)
+
+        self.railbutton = CTkButton(self)
+        self.railbutton.configure(text="Configure Rail...", command=self.rail_window, width=118)
+        self.railbutton.place(anchor="e", relx=0.98, rely=0.11, x=0, y=0)
+        self.railwindow = None
+
         self.configure(border_width=1, corner_radius=0, height=480, width=600)
     
     def environment_window(self):
@@ -118,3 +126,65 @@ class MissionFrame(ctk.CTkFrame):
         except Exception:
             self.envlabel.configure(text="An error has occurred")
             self.envlabel.update()
+    
+    def rail_window(self):
+        if self.railwindow is None or not self.railwindow.winfo_exists():
+            self.railwindow = ctk.CTkToplevel()
+            self.railwindow.title("Configure Rail")
+            self.railwindow.configure(width=250, height=180)
+            self.railwindow.resizable(False, False)
+            self.railwindow.after(
+                201,
+                lambda: self.railwindow.iconphoto(
+                    False, tk.PhotoImage(file=resource_path("icon.png"))
+                ),
+            )
+
+            self.raillengthlabel = CTkLabel(self.railwindow, text="Rail length [m]")
+            self.raillengthlabel.place(anchor="w", relx=0.05, rely=0.14)
+            self.raillengthentry = CTkEntry(self.railwindow, placeholder_text="0", width=118)
+            self.raillengthentry.place(anchor="e", relx=0.95, rely=0.14)
+
+            self.inclinationlabel = CTkLabel(self.railwindow, text="Inclination [deg]")
+            self.inclinationlabel.place(anchor="w", relx=0.05, rely=0.38)
+            self.inclinationentry = CTkEntry(self.railwindow, placeholder_text="0", width=118)
+            self.inclinationentry.place(anchor="e", relx=0.95, rely=0.38)
+
+            self.headinglabel = CTkLabel(self.railwindow, text="Heading [deg]")
+            self.headinglabel.place(anchor="w", relx=0.05, rely=0.62)
+            self.headingentry = CTkEntry(self.railwindow, placeholder_text="0", width=118)
+            self.headingentry.place(anchor="e", relx=0.95, rely=0.62)
+
+            self.setrailbutton = CTkButton(self.railwindow, text="Set", command=self.set_rail, width=90)
+            self.setrailbutton.place(anchor="center", relx=0.75, rely=0.86)
+
+            self.loadrailbutton = CTkButton(self.railwindow, text="Load", command=self.load_rail, width=90)
+            self.loadrailbutton.place(anchor="center", relx=0.25, rely=0.86)
+
+            self.railwindow.after(50, self.railwindow.lift)
+            self.railwindow.after(50, self.railwindow.focus)
+
+        else:
+            self.railwindow.lift()
+            self.railwindow.focus()
+
+    def load_rail(self):
+        try:
+            updateentry(self.raillengthentry, config.rail_length)
+            updateentry(self.inclinationentry, config.inclination)
+            updateentry(self.headingentry, config.heading)
+        except Exception:
+            pass
+
+    def set_rail(self):
+        self.raillabel.configure(text="Setting up rail...")
+        self.raillabel.update()
+        try:
+            config.rail_length = float(self.raillengthentry.get())
+            config.inclination = float(self.inclinationentry.get())
+            config.heading = float(self.headingentry.get())
+            self.raillabel.configure(text="Rail has been configured")
+            self.raillabel.update()
+        except Exception:
+            self.raillabel.configure(text="Rail is not configured")
+            self.raillabel.update()
