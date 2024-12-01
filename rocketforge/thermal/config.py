2f7d0d9d14cc0c8ac05d3d1b14cf7ebaf1ad479b
alessio26gas <alessio26gas@libero.it>
2024-12-01 05:13:05 +0100

    feat: added regenerative cooling global variables

--- rocketforge/thermal/config.py
+++ rocketforge/thermal/config.py
@@ -2,3 +2,55 @@
 film = False
 oxfilm = 0.0
 fuelfilm = 0.0
+
+n_stations = None
+max_iter = None
+tuning_factor = None
+
+gamma = None
+gamma_e = None
+Pr_0 = None
+Pr_t = None
+Pr_e = None
+mu_0 = None
+cp_0 = None
+c_star = None
+p_c = None
+T_c = None
+M_e = None
+
+L_cyl = None
+L_c = None
+L_e = None
+
+shape = 1
+A_t = None
+eps = None
+eps_c = None
+RnOvRt = None
+R1OvRt = None
+R2OvR2max = None
+b = None
+theta = None
+thetan = None
+thetae = None
+
+t_w = None
+lambda_w = None
+eps_w = None
+
+T_ci = None
+p_ci = None
+pcoOvpc = None
+m_dot_c = None
+a1 = None
+a2 = None
+a3 = None
+b1 = None
+b2 = None
+b3 = None
+d1 = None
+d2 = None
+d3 = None
+
+coolant = None
