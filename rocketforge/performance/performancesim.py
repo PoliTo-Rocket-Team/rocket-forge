71367a673adf13c52a83c7dc3d983ad1abc259cf
CeruttiNicolas <ceruttinicolas13@gmail.com>
2024-12-01 20:02:38 +0100

    feat: implemented Quantity in PerformanceSimInput

--- rocketforge/performance/performancesim.py
+++ rocketforge/performance/performancesim.py
@@ -17,19 +17,23 @@ class PerformanceSimInput:
                  optimization_mode = None,
                  inlet_condition_type=None,
                  inlet_condition_value=None,
-                 outlet_condition_type=None,
-                 outlet_condition_value=None,
-                 outlet_condition_value_uom=None
+                 exit_condition_type=None,
+                 exit_condition_value=None,
+                 exit_condition_uom=None,
+                 nominal_thrust=None,
+                 nominal_thrust_uom=None,
+                 design_external_pressure=None,
+                 design_external_pressure_uom=None
                 ):
         self.oxidizer = oxidizer
         self.fuel = fuel
-        self.chamber_pressure = chamber_pressure
-        self.chamber_pressure_uom = chamber_pressure_uom
-        self.mixture_ratio = mixture_ratio
-        self.mixture_ratio_uom = mixture_ratio_uom
-        self.optimization_mode = optimization_mode
-        self.inlet_condition_type = inlet_condition_type
-        self.inlet_condition_value = inlet_condition_value
-        self.outlet_condition_type  = outlet_condition_type 
-        self.outlet_condition_value = outlet_condition_value
-        self.outlet_condition_value_uom = outlet_condition_value_uom
+        self.chamber_pressure = Quantity(name="Chamber Pressure", value=float(chamber_pressure), uom=chamber_pressure_uom)
+        self.mixture_ratio = Quantity(name="Mixture Ratio", value=float(mixture_ratio), uom=mixture_ratio_uom)
+        self.optimization_mode = optimization_mode # 0: Mixture ratio from input, 1: Isp (vac), 2: Isp (opt), 3: Isp (SL)
+        self.inlet_condition_type = inlet_condition_type # 0: Contraction area ratio, 1: Infinite area combustor
+        self.inlet_condition = Quantity(name="Inlet Condition", value=float(inlet_condition_value))
+        self.exit_condition_type = exit_condition_type # 0: Expansion area ratio, 1: Pressure ratio, 2: Exit pressure 
+        self.exit_condition = Quantity(name="Exit Condition", value=float(exit_condition_value), uom=exit_condition_uom)
+        self.nominal_thrust = Quantity(name="Nominal Thrust", value=float(nominal_thrust), uom=nominal_thrust_uom)
+        self.design_external_pressure = Quantity(name="Design External Pressure", value=float(design_external_pressure), uom=design_external_pressure_uom)
+
