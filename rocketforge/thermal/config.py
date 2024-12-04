04775feebda6b48f4e24efb66c7a910d11d1e08f
alessio26gas <alessio26gas@libero.it>
2024-12-04 17:25:44 +0100

    refactor: comments in thermal analysis global variables

--- rocketforge/thermal/config.py
+++ rocketforge/thermal/config.py
@@ -1,13 +1,50 @@
 # Global thermal analysis variables
+
+# Cooling methods
+regen = False
+rad = False
 film = False
-oxfilm = 0.0
-fuelfilm = 0.0
 
-n_stations = None
-max_iter = None
-tuning_factor = None
-stability = None
+# Regenerative cooling parameters
+## Coolant parameters
+coolant = None
+m_dot_c = None
+T_ci = None
+p_ci = None
 
+## Wall properties
+lambda_w = None
+t_w = None
+
+## Channels geometry
+a1 = None
+a2 = None
+a3 = None
+b1 = None
+b2 = None
+b3 = None
+d1 = None
+d2 = None
+d3 = None
+NC = None
+
+## Chamber geometry
+shape = None
+L_cyl = None
+L_c = None
+L_e = None
+A_t = None
+eps = None
+eps_c = None
+RnOvRt = None
+R1OvRt = None
+R2OvR2max = None
+b = None
+theta = None
+thetan = None
+thetae = None
+
+## Thermodynamic properties
 gamma = None
 gamma_e = None
 Pr_0 = None
@@ -20,39 +57,16 @@ p_c = None
 T_c = None
 M_e = None
 
-L_cyl = None
-L_c = None
-L_e = None
+## Advanced options
+pcoOvpc = None
+n_stations = None
+max_iter = None
+tuning_factor = None
+stability = None
 
-shape = 1
-A_t = None
-eps = None
-eps_c = None
-RnOvRt = None
-R1OvRt = None
-R2OvR2max = None
-b = None
-theta = None
-thetan = None
-thetae = None
-
-t_w = None
-lambda_w = None
+# Radiation cooling parameters
 eps_w = None
 
-T_ci = None
-p_ci = None
-pcoOvpc = None
-m_dot_c = None
-a1 = None
-a2 = None
-a3 = None
-b1 = None
-b2 = None
-b3 = None
-d1 = None
-d2 = None
-d3 = None
-NC = None
-
-coolant = None
+# Film cooling parameters
+oxfilm = 0.0
+fuelfilm = 0.0
