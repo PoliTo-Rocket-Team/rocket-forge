f14dda2b5ae93b414bbb58110617821b210a8e56
alessio26gas <alessio26gas@libero.it>
2024-12-05 12:20:04 +0100

    refactor: added performance global variables in thermal model

--- rocketforge/thermal/regenerative.py
+++ rocketforge/thermal/regenerative.py
@@ -1,6 +1,7 @@
 from numpy import *
 import matplotlib.pyplot as plt
 import rocketforge.thermal.config as config
+import rocketforge.performance.config as pconf
 import rocketforge.geometry.convergent as convergent
 import rocketforge.geometry.top as top
 import rocketforge.geometry.conical as conical
@@ -36,7 +37,7 @@ def main():
         delta = generate_profile(x, config.d1, config.d2, config.d3, R)
         NC = C / (a + delta)
     d_e = 2.0 * a * b / (a + b)
-    p = linspace(config.p_ci, config.pcoOvpc * config.p_c, config.n_stations)
+    p = linspace(config.p_ci, config.pcoOvpc * pconf.pc, config.n_stations)
 
     T_aw = (
         config.T_c
@@ -139,7 +140,7 @@ def main():
 
 
 def bartz(M, A, gamma, T_wg):
-    R_t = sqrt(config.A_t / pi)
+    R_t = sqrt(pconf.At / pi)
     sigma = (
         (0.5 * (T_wg / config.T_c) * (1.0 + (gamma - 1.0) / 2.0 * M**2.0) + 0.5)**(-0.68) 
         * (1.0 + (gamma - 1.0) / 2.0 * M**2.0)**(-0.12)
@@ -148,9 +149,9 @@ def bartz(M, A, gamma, T_wg):
         0.026 / (2.0 * R_t) ** 0.2
         * config.mu_0**0.2
         * config.cp_0 / config.Pr_0**0.6
-        * (config.p_c / config.c_star) ** 0.8
+        * (pconf.pc / pconf.cstar_d) ** 0.8
         * (4.0 / (config.RnOvRt + config.R1OvRt)) ** 0.1
-        * (config.A_t / A) ** 0.9
+        * (pconf.At / A) ** 0.9
         * sigma
         / config.tuning_factor
     )
@@ -159,29 +160,29 @@ def bartz(M, A, gamma, T_wg):
 
 def get_geometry():
     XC, YC = convergent.get(
-        config.A_t,
+        pconf.At,
         config.R1OvRt,
         config.L_c,
         radians(config.b),
         config.R2OvR2max,
-        config.eps_c,
+        pconf.epsc,
     )
     if config.shape == 0:
         XD, YD = conical.get(
-            config.A_t,
+            pconf.At,
             config.RnOvRt,
-            config.eps,
+            pconf.eps,
             config.L_e,
             radians(config.theta)
         )
     else:
         XD, YD = top.get(
-            config.A_t,
+            pconf.At,
             config.RnOvRt,
             config.L_e,
             radians(config.thetan),
             radians(config.thetae),
-            config.eps,
+            pconf.eps,
         )
     X = concatenate([XC, XD]) + config.L_c
     Y = concatenate([YC, YD])
@@ -189,9 +190,9 @@ def get_geometry():
 
 
 def generate_profile(x, y_c, y_t, y_e, R):
-    R_t = sqrt(config.A_t / pi)
-    R_e = R_t * sqrt(config.eps)
-    R_c = R_t * sqrt(config.eps_c)
+    R_t = sqrt(pconf.At / pi)
+    R_e = R_t * sqrt(pconf.eps)
+    R_c = R_t * sqrt(pconf.epsc)
     return piecewise(
         x,
         [
