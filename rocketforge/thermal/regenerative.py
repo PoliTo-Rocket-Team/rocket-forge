d908b6d7ae261ddfe292d42ea798bbd749639c8e
alessio26gas <alessio26gas@libero.it>
2024-12-05 18:10:49 +0100

    refacor: regenerative cooling class

--- rocketforge/thermal/regenerative.py
+++ rocketforge/thermal/regenerative.py
@@ -8,135 +8,154 @@ import rocketforge.geometry.conical as conical
 from rocketprops.rocket_prop import get_prop
 
 
-def main():
-    P = get_prop(config.coolant)
-    X, Y = get_geometry()
+class Regen():
+    def run(self):
+        P = get_prop(config.coolant)
+        X, Y = get_geometry()
 
-    L_tot = config.L_c + config.L_e
-    x = linspace(0.0, L_tot, config.n_stations)
-    R = interp(x, X, Y)
-    A = pi * R**2.0
-    A_w = 2.0 * pi * R * L_tot / config.n_stations
+        L_tot = config.L_c + config.L_e
+        x = linspace(0.0, L_tot, config.n_stations)
+        R = interp(x, X, Y)
+        A = pi * R**2.0
+        A_w = 2.0 * pi * R * L_tot / config.n_stations
 
-    M = generate_profile(x, 0.0, 1.0, config.M_e, R)
-    Pr = generate_profile(x, config.Pr_0, config.Pr_t, config.Pr_e, R)
-    gamma = generate_profile(x, config.gamma, config.gamma, config.gamma_e, R)
+        M = generate_profile(x, 0.0, 1.0, config.M_e, R)
+        Pr = generate_profile(x, config.Pr_0, config.Pr_t, config.Pr_e, R)
+        gamma = generate_profile(x, config.gamma, config.gamma, config.gamma_e, R)
 
-    b = generate_profile(x, config.b1, config.b2, config.b3, R)
-    C = 2.0 * pi * (R + config.t_w + b / 2.0)
-    if config.NC:
-        NC = config.NC
-        if config.a1:
-            a = generate_profile(x, config.a1, config.a2, config.a3, R)
-            delta = C / NC - a
+        b = generate_profile(x, config.b1, config.b2, config.b3, R)
+        C = 2.0 * pi * (R + config.t_w + b / 2.0)
+        if config.NC:
+            NC = config.NC
+            if config.a1:
+                a = generate_profile(x, config.a1, config.a2, config.a3, R)
+                delta = C / NC - a
+            else:
+                delta = generate_profile(x, config.d1, config.d2, config.d3, R)
+                a = C / NC - delta
         else:
+            a = generate_profile(x, config.a1, config.a2, config.a3, R)
             delta = generate_profile(x, config.d1, config.d2, config.d3, R)
-            a = C / NC - delta
-    else:
-        a = generate_profile(x, config.a1, config.a2, config.a3, R)
-        delta = generate_profile(x, config.d1, config.d2, config.d3, R)
-        NC = C / (a + delta)
-    d_e = 2.0 * a * b / (a + b)
-    p = linspace(config.p_ci, config.pcoOvpc * pconf.pc, config.n_stations)
+            NC = C / (a + delta)
+        d_e = 2.0 * a * b / (a + b)
+        p = linspace(config.p_ci, config.pcoOvpc * pconf.pc, config.n_stations)
 
-    T_aw = (
-        config.T_c
-        * (1.0 + Pr**0.33 * (gamma - 1.0) / 2.0 * M**2.0)
-        / (1.0 + (gamma - 1.0) / 2.0 * M**2.0)
-    )
+        T_aw = (
+            config.T_c
+            * (1.0 + Pr**0.33 * (gamma - 1.0) / 2.0 * M**2.0)
+            / (1.0 + (gamma - 1.0) / 2.0 * M**2.0)
+        )
 
-    T_wg = full(config.n_stations, config.T_ci)
-    T_wc = full(config.n_stations, config.T_ci)
-    T_c = full(config.n_stations, config.T_ci)
-    cp_c = [P.CpAtTdegR(T_c[i] * 1.8) * 4186.8 for i in range(config.n_stations)]
-    mu_c = zeros(config.n_stations)
-    lambda_c = zeros(config.n_stations)
+        T_wg = full(config.n_stations, config.T_ci)
+        T_wc = full(config.n_stations, config.T_ci)
+        T_c = full(config.n_stations, config.T_ci)
+        cp_c = [P.CpAtTdegR(T_c[i] * 1.8) * 4186.8 for i in range(config.n_stations)]
+        mu_c = zeros(config.n_stations)
+        lambda_c = zeros(config.n_stations)
 
-    iter = 0
-    while True:
-        iter += 1
+        iter = 0
+        while True:
+            iter += 1
 
-        h = bartz(M, A, gamma, T_wg)
-        q = h * (T_aw - T_wg)
-        
-        dT_c = q * A_w / config.m_dot_c / cp_c
-        for i in reversed(range(config.n_stations)):
-            if i != config.n_stations - 1:
-                T_c[i] = T_c[i + 1] + dT_c[i]
-        
-        for i in range(config.n_stations):
-            cp_c[i] = P.CpAtTdegR(T_c[i] * 1.8) * 4186.8
-            mu_c[i] = P.Visc_compressed(T_c[i] * 1.8, p[i] / 6894.75728) / 10.0
-            lambda_c[i] = P.CondAtTdegR(T_c[i] * 1.8) * 1.72958
+            h = bartz(M, A, gamma, T_wg)
+            q = h * (T_aw - T_wg)
+            
+            dT_c = q * A_w / config.m_dot_c / cp_c
+            for i in reversed(range(config.n_stations)):
+                if i != config.n_stations - 1:
+                    T_c[i] = T_c[i + 1] + dT_c[i]
+            
+            for i in range(config.n_stations):
+                cp_c[i] = P.CpAtTdegR(T_c[i] * 1.8) * 4186.8
+                mu_c[i] = P.Visc_compressed(T_c[i] * 1.8, p[i] / 6894.75728) / 10.0
+                lambda_c[i] = P.CondAtTdegR(T_c[i] * 1.8) * 1.72958
 
-        Re_c = config.m_dot_c / a / b / NC * d_e / mu_c
-        Pr_c = mu_c * cp_c / lambda_c
-        Nu = 0.023 * Re_c**0.8 * Pr_c**0.4
-        h_c = Nu * lambda_c / d_e
-        h_c0 = h_c
+            Re_c = config.m_dot_c / a / b / NC * d_e / mu_c
+            Pr_c = mu_c * cp_c / lambda_c
+            Nu = 0.023 * Re_c**0.8 * Pr_c**0.4
+            h_c = Nu * lambda_c / d_e
+            h_c0 = h_c
 
-        for i in range(config.max_iter):
-            xi = sqrt(2.0 * h_c / delta / config.lambda_w) * b
-            eta_f = a / (a + delta) + 2.0 * b / (a + delta) * tanh(xi) / xi
-            hc_old = h_c
-            h_c = h_c0 * eta_f
-            if all(abs((hc_old - h_c)/hc_old) < 0.01):
+            for i in range(config.max_iter):
+                xi = sqrt(2.0 * h_c / delta / config.lambda_w) * b
+                eta_f = a / (a + delta) + 2.0 * b / (a + delta) * tanh(xi) / xi
+                hc_old = h_c
+                h_c = h_c0 * eta_f
+                if all(abs((hc_old - h_c)/hc_old) < 0.01):
+                    break
+
+            T_wc = T_c + q / h_c
+            T_wg_new = T_wc + q * config.t_w / config.lambda_w
+
+            if all(abs((T_wg - T_wg_new) / T_wg) < 0.05) or iter == config.max_iter:
+                T_wg = T_wg_new
                 break
 
-        T_wc = T_c + q / h_c
-        T_wg_new = T_wc + q * config.t_w / config.lambda_w
+            T_wg = (1.0 - config.stability) * T_wg + config.stability * T_wg_new
+        
+        self.x = x
+        self.T_wg = T_wg
+        self.T_wc = T_wc
+        self.T_c = T_c
+        self.q = q
+        self.a = a
+        self.b = b
+        self.delta = delta
+        self.X = X
+        self.Y = Y
 
-        if all(abs((T_wg - T_wg_new) / T_wg) < 0.05) or iter == config.max_iter:
-            T_wg = T_wg_new
-            break
+    def plot_T(self):
+        _, ax = plt.subplots()
+        ax.plot(self.x, self.T_wg, label="Twg")
+        ax.plot(self.x, self.T_wc, label="Twc")
+        ax.plot(self.x, self.T_c, label="Tc")
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("Temperature [K]")
+        ax.grid()
+        ax.legend(loc="upper right")
+        axt = ax.twinx()
+        axt.plot(self.X, self.Y, color="black", label="Thrust chamber contour")
+        axt.set_ylabel("Radius [m]")
+        axt.axis([0, max(self.x), 0, max(self.x)])
+        axt.legend(loc="upper left")
+        plt.title("Temperature distribution")
+        plt.show()
 
-        T_wg = (1.0 - config.stability) * T_wg + config.stability * T_wg_new
+    def plot_q(self):
+        _, ax = plt.subplots()
+        ax.plot(self.x, self.q / 1000, label="Heat flux")
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("Heat flux [kW/m^2]")
+        ax.grid()
+        ax.legend(loc="upper right")
+        axt = ax.twinx()
+        axt.plot(self.X, self.Y, color="black", label="Thrust chamber contour")
+        axt.set_ylabel("Radius [m]")
+        axt.axis([0, max(self.x), 0, max(self.x)])
+        axt.legend(loc="upper left")
+        plt.title("Heat flux distribution")
+        plt.show()
 
-    fig, ax = plt.subplots()
-    ax.plot(x, T_wg, label="Twg")
-    ax.plot(x, T_wc, label="Twc")
-    ax.plot(x, T_c, label="Tc")
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("Temperature [K]")
-    ax.grid()
-    ax.legend(loc="upper right")
-    ax2 = ax.twinx()
-    ax2.plot(X, Y, color="black", label="Thrust chamber contour")
-    ax2.set_ylabel("Radius [m]")
-    ax2.axis([0, max(x), 0, max(x)])
-    ax2.legend(loc="upper left")
-    plt.title("Temperature distribution")
+    def plot_g(self):
+        _, ax = plt.subplots()
+        ax.plot(self.x, self.a, label="a")
+        ax.plot(self.x, self.b, label="b")
+        ax.plot(self.x, self.delta, label="delta")
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("Length [m]")
+        ax.grid()
+        ax.legend(loc="upper right")
+        ax.axis([0, max(self.x), 0, 1.5 * max([max(self.a), max(self.b), max(self.delta)])])
+        axt = ax.twinx()
+        axt.plot(self.X, self.Y, color="black", label="Thrust chamber contour")
+        axt.set_ylabel("Radius [m]")
+        axt.axis([0, max(self.x), 0, max(self.x)])
+        axt.legend(loc="upper left")
+        plt.title("Cooling channels")
+        plt.show()
 
-    fig2, ax3 = plt.subplots()
-    ax3.plot(x, q / 1000, label="Heat flux")
-    ax3.set_xlabel("x [m]")
-    ax3.set_ylabel("Heat flux [kW/m^2]")
-    ax3.grid()
-    ax3.legend(loc="upper right")
-    ax4 = ax3.twinx()
-    ax4.plot(X, Y, color="black", label="Thrust chamber contour")
-    ax4.set_ylabel("Radius [m]")
-    ax4.axis([0, max(x), 0, max(x)])
-    ax4.legend(loc="upper left")
-    plt.title("Heat flux distribution")
-
-    fig3, ax5 = plt.subplots()
-    ax5.plot(x, a, label="a")
-    ax5.plot(x, b, label="b")
-    ax5.plot(x, delta, label="delta")
-    ax5.set_xlabel("x [m]")
-    ax5.set_ylabel("Length [m]")
-    ax5.grid()
-    ax5.legend(loc="upper right")
-    ax5.axis([0, max(x), 0, 1.5 * max([max(a), max(b), max(delta)])])
-    ax6 = ax5.twinx()
-    ax6.plot(X, Y, color="black", label="Thrust chamber contour")
-    ax6.set_ylabel("Radius [m]")
-    ax6.axis([0, max(x), 0, max(x)])
-    ax6.legend(loc="upper left")
-    plt.title("Cooling channels")
-
-    plt.show()
+    def details(self):
+        pass
 
 
 def bartz(M, A, gamma, T_wg):
@@ -204,7 +223,3 @@ def generate_profile(x, y_c, y_t, y_e, R):
             lambda _: y_t * (R_e - R[x > config.L_c]) / (R_e - R_t) + y_e * (R[x > config.L_c] - R_t) / (R_e - R_t),
         ],
     )
-
-
-if __name__ == "__main__":
-    main()
