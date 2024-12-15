02e8ca4a1d8d165d19c9167e132186c80ec2854f
alessio26gas <alessio26gas@libero.it>
2024-12-15 01:21:02 +0100

    refactor: Bartz relation in separated file

--- rocketforge/thermal/regenerative.py
+++ rocketforge/thermal/regenerative.py
@@ -5,6 +5,7 @@ import rocketforge.performance.config as pconf
 import rocketforge.geometry.convergent as convergent
 import rocketforge.geometry.top as top
 import rocketforge.geometry.conical as conical
+from rocketforge.thermal.heat_flux import bartz
 from rocketprops.rocket_prop import get_prop
 
 
@@ -156,25 +157,6 @@ class Regen():
 
     def details(self):
         pass
-
-
-def bartz(M, A, gamma, T_wg):
-    R_t = sqrt(pconf.At / pi)
-    sigma = (
-        (0.5 * (T_wg / config.T_c) * (1.0 + (gamma - 1.0) / 2.0 * M**2.0) + 0.5)**(-0.68) 
-        * (1.0 + (gamma - 1.0) / 2.0 * M**2.0)**(-0.12)
-    )
-    h_gas = (
-        0.026 / (2.0 * R_t) ** 0.2
-        * config.mu_0**0.2
-        * config.cp_0 / config.Pr_0**0.6
-        * (pconf.pc / pconf.cstar_d) ** 0.8
-        * (4.0 / (config.RnOvRt + config.R1OvRt)) ** 0.1
-        * (pconf.At / A) ** 0.9
-        * sigma
-        / config.tuning_factor
-    )
-    return h_gas
 
 
 def get_geometry():
