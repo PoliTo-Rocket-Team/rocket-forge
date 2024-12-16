a789a6266cea24952a4115e357d975b1a2c77155
alessio26gas <alessio26gas@libero.it>
2024-12-16 23:30:16 +0100

    feat: method to print channels geometry in ThermalFrame

--- rocketforge/gui/thermal.py
+++ rocketforge/gui/thermal.py
@@ -225,6 +225,12 @@ class ThermalFrame(ctk.CTkFrame):
         else:
             showwarning(title="Warning", message="Channels geometry unavailable")
 
+    def pring_g(self):
+        if self.regen != None:
+            self.regen.print_g()
+        else:
+            showwarning(title="Warning", message="Channels geometry unavailable")
+
     def details(self):
         if self.regen != None:
             self.regen.details()
