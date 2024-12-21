c5ef05977502e699355b177c02f13c05c843ee15
alessio26gas <alessio26gas@libero.it>
2024-12-22 00:47:51 +0100

    refactor: improved resource_path function code style

--- rocketforge/utils/resources.py
+++ rocketforge/utils/resources.py
@@ -2,14 +2,14 @@ import sys, os
 
 
 def resource_path(relative_path: str) -> str:
-    '''
+    """
     This function is used to obtain the absolute path of a file or resource relative to the main executable's path
-    '''
+    """
     try:
-        '''
+        """
         sys._MEIPASS is set by the PyInstaller module
         When a PyInstaller application is run, this variable contains the path to the temporary directory where the application's assets are extracted
-        '''
+        """
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
